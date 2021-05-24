97dd452b3c9c9ca8ca9460396e0b3eb5525af562
Jun Hee Lee <junheelee@Junui-iMac.local>
2021-05-24 21:40:35 +0900

    #8 Define Mutations

--- graphql/db.js
+++ graphql/db.js
@@ -1,49 +1,49 @@
-export const people = [
+let movies = [
   {
     id: 0,
-    name: "Nicolas",
-    age: 18,
-    gender: "female",
+    name: "Star Wars - The new one",
+    score: 1,
   },
   {
     id: 1,
-    name: "Jisu",
-    age: 18,
-    gender: "female",
+    name: "Avengers - The new one",
+    score: 8,
   },
   {
     id: 2,
-    name: "Japan Guy",
-    age: 18,
-    gender: "male",
+    name: "The Godfather I",
+    score: 99,
   },
   {
     id: 3,
-    name: "Daal",
-    age: 18,
-    gender: "male",
-  },
-  {
-    id: 4,
-    name: "JD",
-    age: 18,
-    gender: "male",
-  },
-  {
-    id: 5,
-    name: "moondaddi",
-    age: 18,
-    gender: "male",
-  },
-  {
-    id: 6,
-    name: "flynn",
-    age: 18,
-    gender: "male",
+    name: "Logan",
+    score: 2,
   },
 ];
 
+export const getMovies = () => movies;
+
 export const getById = (id) => {
-  const filteredPeople = people.filter((person) => person.id === id);
-  return filteredPeople[0];
+  const filteredMovies = movies.filter((movie) => movie.id === id);
+  return filteredMovies[0];
+};
+
+export const deleteMovie = (id) => {
+  const cleanedMovies = movies.filter((movie) => movie.id !== id);
+  if (movies.length > cleanedMovies.length) {
+    movies = cleanedMovies;
+    return true;
+  } else {
+    return false;
+  }
+};
+
+export const addMovie = (name, score) => {
+  const newMovie = {
+    id: `${movies.length + 1}`,
+    name,
+    score,
+  };
+  movies.push(newMovie);
+  return newMovie;
 };
