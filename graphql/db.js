3eb4233a90742939b5b66283acde360b43b042a3
junhee_lee2 <junhee_lee2@tmax.co.kr>
2021-05-24 17:08:59 +0900

    #7 Creating Queries with Arguments

--- graphql/db.js
+++ graphql/db.js
@@ -1,42 +1,42 @@
 export const people = [
   {
-    id: "0",
+    id: 0,
     name: "Nicolas",
     age: 18,
     gender: "female",
   },
   {
-    id: "1",
+    id: 1,
     name: "Jisu",
     age: 18,
     gender: "female",
   },
   {
-    id: "2",
+    id: 2,
     name: "Japan Guy",
     age: 18,
     gender: "male",
   },
   {
-    id: "3",
+    id: 3,
     name: "Daal",
     age: 18,
     gender: "male",
   },
   {
-    id: "4",
+    id: 4,
     name: "JD",
     age: 18,
     gender: "male",
   },
   {
-    id: "5",
+    id: 5,
     name: "moondaddi",
     age: 18,
     gender: "male",
   },
   {
-    id: "6",
+    id: 6,
     name: "flynn",
     age: 18,
     gender: "male",
@@ -44,6 +44,6 @@ export const people = [
 ];
 
 export const getById = (id) => {
-  const filteredPeople = people.filter((person) => people.id === id);
+  const filteredPeople = people.filter((person) => person.id === id);
   return filteredPeople[0];
 };
