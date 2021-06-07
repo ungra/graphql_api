fafed9bb7e8f3552e7bdfd978a28b811e4d3cc06
junhee_lee2 <junhee_lee2@tmax.co.kr>
2021-06-07 14:30:57 +0900

    bugfix : db.js

--- graphql/db.js
+++ graphql/db.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 const BASE_URL = "https://yts.am/api/v2/";
-const LIST_MOVIES_URL = "https://yts.mx/api/v2/list_movies.json?q";
-const MOVIE_DETAILS_URL = "https://yts.mx/api/v2/movie_details.json";
-const MOVIE_SUGGESTIONS_URL = "https://yts.mx/api/v2/movie_suggestions.json";
+const LIST_MOVIES_URL =
+  "https://yts.mx/api/v2/list_movies.json?q";
+const MOVIE_DETAILS_URL =
+  "https://yts.mx/api/v2/movie_details.json";
+const MOVIE_SUGGESTIONS_URL =
+  "https://yts.mx/api/v2/movie_suggestions.json";
 
 export const getMovies = async (limit, rating) => {
   const {
@@ -11,8 +14,9 @@ export const getMovies = async (limit, rating) => {
     },
   } = await axios(LIST_MOVIES_URL, {
     params: {
-      limit, minimum_rating: rating
-    }
+      limit,
+      minimum_rating: rating,
+    },
   });
   return movies;
 };
@@ -24,21 +28,21 @@ export const getMovie = async (id) => {
     },
   } = await axios(MOVIE_DETAILS_URL, {
     params: {
-      movies_id: id,
+      movie_id: id,
     },
   });
   return movie;
 };
 
-export const getSuggestions = async id => {
+export const getSuggestions = async (id) => {
   const {
     data: {
-      data: {movies}
-    }
+      data: { movies },
+    },
   } = await axios(MOVIE_SUGGESTIONS_URL, {
     params: {
-      movie_id: id
-    }
-  })
-  return movies
-}
+      movie_id: id,
+    },
+  });
+  return movies;
+};
